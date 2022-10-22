863eba54be0d5350cc5865b1707271eafb5533c4
mirajulalam <alifmirajulalam@gmail.com>
2022-10-22 11:05:39 -0700

    simple ui design added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
+
+            <div class="max-w-md mx-auto mt-10 space-y-5">
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+                    <div class="text-2xl font-semibold" id="counter">0</div>
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                            id="decrement"
+                        >
+                            Decrement
+                        </button>
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                            id="increment"
+                        >
+                            Increment
+                        </button>
+                    </div>
+                </div>
+
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+                    <div class="text-2xl font-semibold" id="counter2">0</div>
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                            id="decrement2"
+                        >
+                            Decrement
+                        </button>
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                            id="increment2"
+                        >
+                            Increment
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
   );
 }
 
